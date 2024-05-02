04daf5a7f2a92252a4c32035eed8e9f163e5b537
tomersi2 <sivantomer10@gmail.com>
2024-05-02 21:31:41 +0300

    Launch file setup

--- install/my_robot_controller/share/my_robot_controller/launch/start_robot.launch.py
+++ install/my_robot_controller/share/my_robot_controller/launch/start_robot.launch.py
@@ -3,15 +3,17 @@ from launch import LaunchDescription
 from launch_ros.actions import Node
 
 def generate_launch_description():
-    urdf_file = os.path.join(os.getcwd(), 'src', 'my_robot_controller', 'description', 'my_robot.urdf')
+    # Absolute path to the URDF file
+    urdf_file = '/home/tomersi2/ros2_ws/src/my_robot_controller/description/my_robot.urdf'
 
+    # Launch description
     return LaunchDescription([
-        # Load URDF file
+        # Robot State Publisher
         Node(
             package='robot_state_publisher',
             executable='robot_state_publisher',
             name='robot_state_publisher',
             output='screen',
             parameters=[{'use_sim_time': False, 'robot_description': open(urdf_file).read()}]
-        )
+        ),
     ])
